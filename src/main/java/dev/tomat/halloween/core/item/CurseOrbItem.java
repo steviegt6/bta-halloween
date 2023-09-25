b9de618b2f8d2135a8b1e44b0389f7c1a28316f9
Tomat <xxlennygamerxx@gmail.com>
2023-09-24 22:55:59 -0700

    Curse Orb throws snowballs for now.

--- src/main/java/dev/tomat/halloween/core/item/CurseOrbItem.java
+++ src/main/java/dev/tomat/halloween/core/item/CurseOrbItem.java
@@ -1,11 +1,27 @@
 package dev.tomat.halloween.core.item;
 
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.entity.projectile.EntitySnowball;
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
 
 public class CurseOrbItem extends Item {
     public CurseOrbItem(int id) {
         super(id);
     }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+        itemstack.consumeItem(entityplayer);
+        world.playSoundAtEntity(entityplayer, "random.bow", 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+        if (!world.isClientSide) {
+            world.entityJoinedWorld(new EntitySnowball(world, entityplayer));
+        }
+
+        return itemstack;
+    }
+
 
     @Override
     public int getColorFromDamage(int i) {
